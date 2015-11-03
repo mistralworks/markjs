7f333193aa25048f8ac383f8a715d353e0210440
Yoza Wiratama <Yoza Wiratama>
2015-11-03 14:39:33 +0700

    preparation for mk-list

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # markjs
 mark is a javascript mvvm framework, easy to templating and data binding.
+
+# Pilosophy
+
+# Development Target
+1. Mark (V) <= v0.0.1
+2. Text (V) <= v0.0.1
+3. Template (V) <= v0.0.1
+4. input (V) <= v0.0.1
+5. Checkbox and Radio (V) <= v0.0.1
+6. List (ongoing) <= v0.0.2 at 07 November 2015
+7. Todo <= v0.0.3 at 14 November 2015
+
+#how to use
+Use `mark.js` in html :
+`<script src="mark.js"></script>`
+Mark is a global variable that will reactive any subscriber and update that data or render it.
+
+## Template
+This Will render `Hello World`
+    <div mk="testTmpl"></div>
+    <template name="testTmpl">Hello World</template>
+Template like controller-like in mvc. `template` have `name` attribute, that will be a token to render template in a html element with attribute `mk` with same value.
+
+## Text
+Simple use `Mark.set('name', 'value');` and render it on `mk-text`
+    <b mk="coolTmpl"></b>
+    <template name="coolTmpl">Hello, <span mk-text="Name"></span></template>
+    <script>
+        Mark.set('Name', 'Yoza');
+    </script>
+Open your console make type `Mark.set('Name', 'Dhika')` it will update `mk-text="Name"`.
+
+## Input
+Input provided by two mark for now, that are `mk-val` and `mk-check`.
+    <div mk="inputTmpl"></div>
+    <template name="inputTmpl">
+        <input mk-value="newItem" type="text" />
+        <input mk-check="cbx" type="checkbox" />
+    </template>
+    <script>
+        Mark.set('newItem', 'test');
+        Mark.set('cbx', true);
+    </script>
+
+----
+#LICENSE
+----
+The MIT License (MIT)
+
+Copyright (c) 2015 Mistralworks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+---
--- mark.js
+++ mark.js
@@ -3,39 +3,39 @@
  */
 
 (function () {
-    //std
-    var tIdLength = 8;
-    //global variable for template and Mark
-    Template = {};
-    Mark = {};
-    //doc manipulation
-    var page = document.documentElement.innerHTML; //html page 1st when iniatiated
-    //    console.log(page);
-    Mark.set = function (property, value) {
-        Mark[property] = value;
-        renderPage();
-    }
+        //std
+        var tIdLength = 8;
+        //global variable for template and Mark
+        Template = {};
+        Mark = {};
+        //doc manipulation
+        var page = document.documentElement.innerHTML; //html page 1st when iniatiated
+        //    console.log(page);
+        Mark.set = function (property, value) {
+            Mark[property] = value;
+            renderPage();
+        }
 
-    Mark.get = function (property) {
-        return Mark[property];
-    }
+        Mark.get = function (property) {
+            return Mark[property];
+        }
 
-    //do anything when content loaded
-    document.addEventListener('DOMContentLoaded', function () {
-        renderPage();
-    }, false);
+        //do anything when content loaded
+        document.addEventListener('DOMContentLoaded', function () {
+            renderPage();
+        }, false);
 
-    //render page
-    function renderPage() {
-        //init all template in page
-        var tmpl = document.getElementsByTagName('template');
-        for (var ii = 0; ii < tmpl.length; ii++) {
-            var d = {
+        //render page
+        function renderPage() {
+            //init all template in page
+            var tmpl = document.getElementsByTagName('template');
+            for (var ii = 0; ii < tmpl.length; ii++) {
+                var d = vk
                 HTML: tmpl[ii].innerHTML,
-                Helpers: {},
-                Events: {},
-                Elements: {},
-                Name: getTemplateName(tmpl[ii])
+                    Helpers: {},
+                    Events: {},
+                    Elements: {},
+                    Name: getTemplateName(tmpl[ii])
             };
             Template[getTemplateName(tmpl[ii])] = d;
             renderTemplate(d.Name, d.HTML);
@@ -46,6 +46,7 @@
         var allElements = document.getElementsByTagName('*');
         for (var i = 0, n = allElements.length; i < n; i++) {
             if (allElements[i].getAttribute("mk") !== null) {
+                //for each mark template
                 if (allElements[i].getAttribute("mk") === name) {
                     allElements[i].innerHTML = template;
                     //mk-text
@@ -57,13 +58,34 @@
                         else updateMKText(name, sName, "");
                     }
                     //mk-value
-                    var nodeMKModel = document.querySelectorAll('[mk="' + name + '"] [mk-value]');
-                    for (var j = 0; j < nodeMKModel.length; j++) {
-                        var sName = nodeMKModel[j].getAttribute('mk-value');
+                    var nodeMKValue = document.querySelectorAll('[mk="' + name + '"] [mk-value]');
+                    for (var j = 0; j < nodeMKValue.length; j++) {
+                        var sName = nodeMKValue[j].getAttribute('mk-value');
                         if (Mark[sName])
                             updateMKValue(name, sName, Mark[sName]);
                         else updateMKValue(name, sName, "");
                     }
+
+                    //mk-check
+                    var nodeMKCheck = document.querySelectorAll('[mk="' + name + '"] [mk-check]');
+                    for (var j = 0; j < nodeMKCheck.length; j++) {
+                        var sName = nodeMKCheck[j].getAttribute('mk-check');
+                        if (Mark[sName])
+                            updateMKCheck(name, sName, Mark[sName]);
+                        else updateMKCheck(name, sName, false);
+                    }
+
+                    //mk-list
+                    var nodeMKList = document.querySelectorAll('[mk="' + name + '"] [mk-list]');
+                    for (var j = 0; j < nodeMKList.length; j++) {
+                        var sName = nodeMKList[j].getAttribute('mk-list');
+                        if (Mark[sName])
+                            updateMKList(name, sName, Mark[sName]);
+                        else updateMKList(name, sName, []);
+                    }
+
+                    //events
+
                 }
 
             }
@@ -82,6 +104,35 @@
         var nodes = document.querySelectorAll('[mk="' + tmplName + '"] [mk-value="' + txtName + '"]');
         for (i = 0; i < nodes.length; ++i) {
             nodes[i].value = value;
+        }
+    };
+
+    function updateMKCheck(tmplName, txtName, value) {
+        var nodes = document.querySelectorAll('[mk="' + tmplName + '"] [mk-check="' + txtName + '"]');
+        for (i = 0; i < nodes.length; ++i) {
+            nodes[i].checked = value;
+        }
+    };
+
+    function updateMKList(tmplName, txtName, value) {
+        var nodes = document.querySelectorAll('[mk="' + tmplName + '"] [mk-list="' + txtName + '"]');
+        var item;
+        for (i = 0; i < nodes.length; ++i) {
+            for (var v = 0; v < value.length; v++) {
+                var items = nodes[i].querySelectorAll('[mk-list-item]');
+                var item = document.createElement("li");
+                item.innerHTML = 'hore';
+                nodes[i].appendChild(item);
+                //                for (j = 0; j < items.length; ++j) {
+                //                    var texts = items[j].querySelectorAll('[mk-text]');
+                //                    item = items[j];
+                //                    for (k = 0; k < texts.length; ++k) {
+                //                        texts[k].innerHTML = value[v][texts[k].getAttribute('mk-text')];
+                //                    }
+                //
+                //                }
+
+            }
         }
     };
 
